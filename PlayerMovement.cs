ad241a8ee6cea5d525c98e6466e7eac34dcf576d
Filipeoliveirap <filipejpo@gmail.com>
2024-11-27 13:26:09 -0300

    Resolvendo o pulo

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,14 +1,17 @@
-
-public class PlayerMovement : MonoBehaviour{
-
+public class PlayerMovement : MonoBehaviour
+{
     [Header("Player Settings")]
     public float speed = 5f;
     public float jumpForce = 10f;
+
     private Rigidbody2D rb;
     private bool isGrounded = true;
+
+    [Header("Ground Check Settings")]
     public Transform groundCheck;  
     public float groundCheckRadius = 0.2f;  
     public LayerMask groundLayer;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -17,8 +20,8 @@ public class PlayerMovement : MonoBehaviour{
     private void Update()
     {
         HandleMovement();
+        HandleGroundCheck();
         HandleJump();
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 
     private void HandleMovement()
@@ -27,37 +30,24 @@ public class PlayerMovement : MonoBehaviour{
         rb.velocity = new Vector2(move * speed, rb.velocity.y);
     }
 
+    private void HandleGroundCheck()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
     private void HandleJump()
     {
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            isGrounded = false;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-        else if (collision.gameObject.CompareTag("Obstacle"))
+        if (collision.gameObject.CompareTag("Obstacle"))
         {
             GameManager.Instance.GameOver();
         }
     }
-
-    private void OnCollisionStay2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("Ground")){
-            isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision){
-        if (collision.gameObject.CompareTag("Ground")){
-            isGrounded = false;
-        }
-    }
-
 }
