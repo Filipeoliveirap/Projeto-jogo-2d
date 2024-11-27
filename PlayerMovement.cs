f255fd404fb01ec5d997f2387ba44047d317458c
Filipeoliveirap <filipejpo@gmail.com>
2024-11-27 13:35:12 -0300

    Resolvendo o pulo

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Player Settings")]
@@ -7,12 +5,7 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 10f;
 
     private Rigidbody2D rb;
-    private bool isGrounded;
-
-    [Header("Ground Check Settings")]
-    public Transform groundCheck;
-    public float groundCheckRadius = 0.2f;
-    public LayerMask groundLayer;
+    private bool isGrounded = true;
 
     private void Start()
     {
@@ -22,7 +15,6 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         HandleMovement();
-        HandleGroundCheck();
         HandleJump();
     }
 
@@ -32,30 +24,41 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(move * speed, rb.velocity.y);
     }
 
-    private void HandleGroundCheck()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-
-        
-        Debug.Log("Is Grounded: " + isGrounded);
-    }
-
     private void HandleJump()
     {
+        // Verifica se o jogador apertou o botão de pulo e se está no chão
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            Debug.Log("Jump Triggered!");
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            isGrounded = false;  // Impede pular novamente até estar no chão
         }
     }
 
-    private void OnDrawGizmosSelected()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-        if (groundCheck != null)
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+            isGrounded = true;  // Marca o jogador como no chão
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            GameManager.Instance.GameOver();  // Notifica o GameManager se colidir com obstáculo
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;  // Garante que o jogador esteja no chão durante o contato
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)  // Corrigido o nome do método
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;  // Marca o jogador como fora do chão quando sair do contato
         }
     }
 }
